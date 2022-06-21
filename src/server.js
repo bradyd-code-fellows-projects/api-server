bcb7b0ea6ae1ed92976224df895a9d2ecbd3692e
Brady <brady.davenport@pm.me>
2022-06-20 20:45:02 -0600

    refactor

--- src/server.js
+++ src/server.js
@@ -8,18 +8,17 @@ const notFound = require('./error-handlers/404');
 const errorHandler = require('./error-handlers/500');
 
 const app = express();
-app.use(logger);
-app.use(express.json());
-
-app.use(foodRoute);
-app.use(vehicleRoute);
 
 const PORT = process.env.PORT || 3002;
+
+app.use(logger);
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('This is the server for Brady\'s 401-lab04');
 });
-
+app.use(foodRoute);
+app.use(vehicleRoute);
 app.use('*', notFound);
 app.use(errorHandler);
 
